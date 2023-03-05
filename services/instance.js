9cd2d4a9e1640b9359ea6b6e88e2dfdd38b8f810
dhiransapkota45 <dhiransapkota55@gmail.com>
2023-03-05 16:37:41 +0545

    commiit

--- services/instance.js
+++ services/instance.js
@@ -1,11 +1,12 @@
 import axios from "axios";
 
 const baseurl = "https://chat-back-production-53b7.up.railway.app/";
+const token = localStorage.getItem("accessToken");
 const api = axios.create({
   baseURL: baseurl,
   headers: {
     "Content-type": "application/json",
-    accessToken: localStorage.getItem("accessToken"),
+    accessToken: token,
   },
 });
 
